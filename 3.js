c7333035d3a39b1376b1626773019be9a627c367
Evan-CSE <53369789+Evan-CSE@users.noreply.github.com>
2020-07-20 00:57:20 +0600

    Task 3

---
+++ 3.js
@@ -0,0 +1,15 @@
+function Click()
+{
+	document.getElementById('Hll').innerHTML='Hello World';
+}
+
+
+function H()
+{
+	document.getElementById('Hll').innerHTML = 'Mouse Hovered... Hello World';
+}
+
+function Press()
+{
+	document.getElementById('Hll').innerHTML = 'Key Pressed... Hello World';
+}
